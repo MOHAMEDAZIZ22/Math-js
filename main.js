49182fe560921aa3613d3f6f85c6eaefc5524543
aziz <erazizeee@gmail.com>
2022-09-08 12:18:10 +0530

    MATH-js

---
+++ main.js
@@ -0,0 +1,17 @@
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.6));
+document.getElementById("pi").innerHTML = Math.PI;
+document.getElementById("e").innerHTML = Math.E;
+document.getElementById("hi").innerHTML = Math.round(4.6);
+document.getElementById("hi1").innerHTML = Math.ceil(3.4);
+document.getElementById("hi2").innerHTML = Math.floor(-8.5);
+document.getElementById("hi3").innerHTML = Math.max(0, 150, 30, 20, -8, -200);
+document.getElementById("hi4").innerHTML = Math.random();
+document.getElementById("hi5").innerHTML = Math.abs(-5.5);
+document.getElementById("hi6").innerHTML = Math.sqrt(64);
+document.getElementById("hi7").innerHTML = Math.pow(8, 2);
+document.getElementById("hi8").innerHTML = Math.trunc(8.9);
+document.getElementById("hi9").innerHTML = Math.sign(-5);
+document.getElementById("hi10").innerHTML = Math.sin(90 * Math.PI / 180);
+document.getElementById("hi11").innerHTML = Math.cos(0 * Math.PI / 180);
+
